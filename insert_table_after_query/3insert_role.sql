802e6cb6cbaa110a88396f32821bb178e74f9446
Farrel Emerson <82019030+Dvizio@users.noreply.github.com>
2022-06-10 20:06:27 +0700

    Add files via upload

---
+++ insert_table_after_query/3insert_role.sql
@@ -0,0 +1,4 @@
+insert into ROLE (id, name, description) values (1, 'Supplier', 'Mengurus ketersediaan barang');
+insert into ROLE (id, name, description) values (2, 'Distributor', 'Mengurus transport barang');
+insert into ROLE (id, name, description) values (3, 'Relawan', 'Menjadi pengurus suatu event');
+insert into ROLE (id, name, description) values (4, 'Pengawas', 'Mengawasi jalannya event');
